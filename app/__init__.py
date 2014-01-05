af5525a859f3af258fc7f3a4158eaf0e02e1797f
Ken H <simechkh@gmail.com>
2014-01-04 19:27:43 -0800

    5 complete and correct

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views, models
-
 import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
@@ -14,4 +12,8 @@ from config import basedir
 
 lm = LoginManager()
 lm.init_app(app)
+lm.login_view = 'login'
+
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
--- app/views.py
+++ app/views.py
@@ -8,11 +8,16 @@ from models import User, ROLE_USER, ROLE_ADMIN
 def load_user(id):
     return User.query.get(int(id))
 
+@app.before_request
+def before_request():
+    g.user = current_user
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = { 'nickname': 'Miguel' } # fake user
-    posts = [ # fake array of posts
+    user = g.user
+    posts = [
         {
             'author': { 'nickname': 'John' },
             'body': 'Beautiful day in Portland!'
@@ -22,7 +27,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template("index.html",
+    return render_template('index.html',
         title = 'Home',
         user = user,
         posts = posts)
@@ -40,3 +45,24 @@ def login():
         title = 'Sign In',
         form = form,
         providers = app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email = resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
